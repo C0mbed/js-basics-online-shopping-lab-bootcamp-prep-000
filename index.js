f9fe827132174bd9b1b24b7a9c25139097533574
Matt Farncombe <matt@farncombe.co>
2020-04-18 21:52:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,7 +57,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber) {
-
+    let final = total();
+    cart = [];
+    return `Your total cost is $${final}, which will be charged to the card ${cardNumber}.`
   } else {
     print "Sorry, we don't have a credit card on file for you."
   }
